bcd16ac73b6e0233bf2e544479c01f6cbade1c68
LukeBeaumont <lbeaumont50@msn.com>
2022-12-28 18:11:59 +0000

    Change computer choice back to using an array as i found why it wasnt orginally working

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,32 +1,23 @@
 let playerSelection = prompt ("Rock, Paper or Scissors?").toLowerCase()
 
-let generateComputerChoice = Math.floor(Math.random() * 3)
+let options = ['rock','paper','scissors']
+
+let computerChoice = options [Math.floor(Math.random()*options.length)]
 
 
-function getComputerChoice(){
-if (generateComputerChoice==0) {
-    return ("rock"); 
-} else if (generateComputerChoice==1) {
-    return ("scissors");
-} else {
-    return ("paper");
-}
-}
-
-let computerChoice = getComputerChoice();
 console.log (computerChoice)
 
 function getWinner() {
     if (playerSelection == "rock" && computerChoice == "scissors" || 
                 playerSelection == "scissors" && computerChoice == "paper" ||
                 playerSelection == "paper" && computerChoice == "rock") {
-                    alert (`computer chose ${computerChoice}. You Win!!`);
+                    alert (`Computer chose ${computerChoice}. You Win!!`);
 
     }  else if (playerSelection==computerChoice) {
-        alert(`Computer chose ${computerChoice}.It's a draw!`);
+        alert(`Computer chose ${computerChoice}. It's a draw!`);
 
     }   else {
-        alert(`computer chose ${computerChoice}. You Lose!`);
+        alert(`Computer chose ${computerChoice}. You Lose!`);
  }
 }
 alert (getWinner())
