3662e79f330e954d30374cc04d1ab7cc26bb2540
LukeBeaumont <lbeaumont50@msn.com>
2022-12-28 12:33:02 +0000

    Make functionthat generates computers choice

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,1 +1,15 @@
 let playerSelection = prompt("Rock, Paper or Scissors?")
+
+let options = ["Apples","Bananas","Pears"];
+  
+function getComputerChoice (){ 
+    let computerChoice = options[Math.floor(Math.random()*options.length)];
+    return computerChoice ;
+}
+
+
+  console.log (getComputerChoice())
+
+
+
+
