10259aa79b041b815c431d4822fa617ad61dbaed
LukeBeaumont <lbeaumont50@msn.com>
2022-12-29 09:33:20 +0000

    Changed the way computer choice was generated into a function for easier future use

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,23 +1,24 @@
-let playerSelection = prompt ("Rock, Paper or Scissors?").toLowerCase()
-
-let options = ['rock','paper','scissors']
-
-let computerChoice = options [Math.floor(Math.random()*options.length)]
-
-
-console.log (computerChoice)
+function getComputerChoice() {
+    let options = ["rock", "paper", "scissors"];
+    let random = options[Math.floor(Math.random()*options.length)];
+    return random;
+   }
 
 function getWinner() {
     if (playerSelection == "rock" && computerChoice == "scissors" || 
                 playerSelection == "scissors" && computerChoice == "paper" ||
                 playerSelection == "paper" && computerChoice == "rock") {
                     alert (`Computer chose ${computerChoice}. You Win!!`);
+                  
 
     }  else if (playerSelection==computerChoice) {
-        alert(`Computer chose ${computerChoice}. It's a draw!`);
+        alert(`Computer chose ${computerChoice}. It's a Draw!`);
+        
 
     }   else {
         alert(`Computer chose ${computerChoice}. You Lose!`);
+        
  }
 }
 alert (getWinner())
+
