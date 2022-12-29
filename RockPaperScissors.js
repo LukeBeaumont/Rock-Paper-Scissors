e0fb712d6a7ae146c388d8fdb4a1152e84d80952
LukeBeaumont <lbeaumont50@msn.com>
2022-12-29 12:56:39 +0000

    Add playerScore and computerScore variables. Make function to loop 5 games

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let options = ["rock", "paper", "scissors"];
     let random = options[Math.floor(Math.random()*options.length)];
@@ -10,13 +13,20 @@ function getComputerChoice() {
     } else if (playerSelection==="paper" && computerSelection==="rock" ||
                playerSelection==="rock" && computerSelection==="scissors" ||
                playerSelection==="scissors" && computerSelection==="paper"){
+                playerScore++;
                 return (`you win! computer chose ${computerSelection}`);
     } else {
+        computerScore++;
         return (`you lose. computer chose ${computerSelection}`)
     }
    }
 
-   let playerSelection = prompt("rock paper scissors?").toLowerCase();
-   let computerSelection = getComputerChoice();
-
-   console.log(playRound(computerSelection,playerSelection))
+   function game() {
+    for (let i = 0; i < 5; i++){ 
+        let computerSelection = getComputerChoice();
+        let playerSelection = prompt("rock paper scissors?").toLowerCase();
+        console.log(playRound(computerSelection,playerSelection));
+        console.log(`Player score:${playerScore}. Computer score:${computerScore}`);
+    }}
+    game()
+    
