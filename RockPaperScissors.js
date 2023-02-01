193a8bfe1d587134aff2d12d4d82773f449cfc41
LukeBeaumont <lbeaumont50@msn.com>
2023-02-01 20:38:33 +0000

    fix gameReset

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -7,7 +7,9 @@ choice.forEach((weapon)=>
   weapon.addEventListener("click", ()=> {
     if(weapon.id){
         playRound(getComputerChoice(),weapon.id);
+        console.log (playerScore, computerScore)
         checkWinner();
+        gameReset();
     }}
   ))
 
@@ -15,15 +17,12 @@ function updateInfo (gameResult){
   document.querySelector(".result-info").textContent = gameResult;
 }
 
-
-
 function updateScore (playerScore,computerScore) {
   document.querySelector(".player-score").textContent = `You: ${playerScore}`;
   document.querySelector(".computer-score").textContent = `Computer: ${computerScore}`;
 }
 
 function getComputerChoice() {
-    //update DOM with computers choice 
     let options = ["rock", "paper", "scissors"];
     let computerChoice = options[Math.floor(Math.random()*options.length)];
     return computerChoice;
@@ -33,9 +32,9 @@ function playRound (computerSelection,playerSelection){
   if (playerSelection===computerSelection){
         updateInfo(`It's a draw. Computer chose ${computerSelection}.`);
   } else if 
-      (playerSelection==="paper" && computerSelection==="rock" ||
-       playerSelection==="rock" && computerSelection==="scissors" ||
-       playerSelection==="scissors" && computerSelection==="paper"){
+      (playerSelection === "paper" && computerSelection === "rock" ||
+       playerSelection === "rock" && computerSelection === "scissors" ||
+       playerSelection === "scissors" && computerSelection === "paper"){
          playerScore ++;
         updateInfo(`You win! Computer chose ${computerSelection}.`);        
   } else {
@@ -43,14 +42,20 @@ function playRound (computerSelection,playerSelection){
         updateInfo(`You lose. Computer chose ${computerSelection}.`);
      }   
      updateScore(playerScore,computerScore);
+     
    }
 
 function checkWinner() { 
-  if (playerScore == 5) {
-    document.querySelector(".result-info").textContent = "Congratulations! You are the first to win 5 rounds!";
-  } else if (computerScore == 5) {
-    document.querySelector(".result-info").textContent = "Unlucky! The computer won 5 rounds.";
+  if (playerScore === 5) {
+     document.querySelector(".result-info").textContent = "Congratulations! You are the first to win 5 rounds!";
+  } else if (computerScore === 5) {
+         document.querySelector(".result-info").textContent = "Unlucky! The computer won 5 rounds.";
   } else return;
 }
   
+function gameReset() {
+    if (playerScore === 5 || computerScore === 5) {
+        document.querySelector(".result-info").textContent += " Want to play again?";
+    } else return; 
+}
    
